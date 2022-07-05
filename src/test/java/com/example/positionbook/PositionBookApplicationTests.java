cac67639d631ba7db7b30fd00c0bb62b1871bc3c
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-07-05 13:11:30 +0100

    start of tests

--- src/test/java/com/example/positionbook/PositionBookApplicationTests.java
+++ src/test/java/com/example/positionbook/PositionBookApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.positionbook;
 
+import com.example.positionbook.controllers.PositionBookController;
+import com.example.positionbook.services.Events;
 import com.example.positionbook.services.Positions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -19,7 +22,27 @@ class PositionBookApplicationTests {
 
 	@Test
 	void testTradeEventEndPoint(){
-		assertArrayEquals(true, true);
+		PositionBookController pbcTest = new PositionBookController();
+		String newEventsJson = new String("{\n" +
+				"    \"Events\": [\n" +
+				"        {\n" +
+				"            \"ID\": 1,\n" +
+				"            \"Action\": \"BUY1\",\n" +
+				"            \"Account\": \"ACC1\",\n" +
+				"            \"Security\": \"SEC1\",\n" +
+				"            \"Quantity\": 12\n" +
+				"        },\n" +
+				"        {\n" +
+				"            \"ID\": 2,\n" +
+				"            \"Action\": \"BUY1\",\n" +
+				"            \"Account\": \"ACC1\",\n" +
+				"            \"Security\": \"SEC1\",\n" +
+				"            \"Quantity\": 24\n" +
+				"        }\n" +
+				"    ]\n" +
+				"}");
+		pbcTest.sendTradeEvents(newEventsJson);
+		assertArrayEquals(positionsTest., true);
 	}
 
 }
